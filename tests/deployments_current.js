cfecb58f21ac52859ce472b8dd4a2dfa13da4675
Mitch Fierro <sendtofierro@gmail.com>
2017-01-03 16:12:41 -0800

    add test for fully unified config output

--- tests/deployments_current.js
+++ tests/deployments_current.js
@@ -4,14 +4,15 @@ var assert = require('assert');
 var fs = require('fs');
 var assert = require('assert');
 var path = require('path')
-
+const util = require('util');
 
 describe('library basic functions', function () {
   before((done) => {
     var http = require('http');
     const PORT=9090;
     function handleRequest(request, response){
-      response.end((require('../config_dir/sample__deployments_response.js')));
+      console.log(JSON.stringify(require('./configdir/sample_deployments_response.js')));
+      response.end(JSON.stringify(require('./configdir/sample_deployments_response.js')));
     }
     var server = http.createServer(handleRequest);
     server.listen(PORT, function(){
@@ -20,12 +21,22 @@ describe('library basic functions', function () {
     });
   })
 
-  it('assembles a configuration correctly', function (done) {
+  it('stitches together bundles correctly', function (done) {
     var Apid = require('../lib/apid');
     var apidLib = new Apid();
     const stitchedConfig = apidLib.stitch(require('./configdir/sample_deployments_response.js'));
-    console.log('Stitched config:\n', stitchedConfig)
+    console.log('Stitched config:\n', util.inspect(stitchedConfig, {depth: null }))
     assert.equal(stitchedConfig, fs.readFileSync(path.join(__dirname, './output-expected')));
     done();
+  })
+
+  it('returns the fully unified correct configuration object, including system config', function (done) {
+    var Apid = require('../lib/apid');
+    var apidLib = new Apid();
+    apidLib.get({systemConfigPath: path.join(__dirname, 'configdir/systemConfig.yaml')}, (err, stitchedConfig) => {
+      console.log('Unified config:\n', util.inspect(stitchedConfig, {depth: null }));
+      //TODO do some checks here
+      done();
+    });
   })
 });
