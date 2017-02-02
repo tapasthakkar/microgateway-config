bc573be9b4b4b3ffe1af610a58bcd821bfcde490
Matthew Dobson <mdobson4@gmail.com>
2017-02-02 09:42:06 -0500

    Update tests to pass using the new parameterized version of get function.

--- tests/deployments_current.js
+++ tests/deployments_current.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 var assert = require('assert');
 var path = require('path')
 const util = require('util');
+const PORT=9090;
 
 describe('library basic functions', function () {
   before((done) => {
     var http = require('http');
-    const PORT=9090;
     function handleRequest(request, response){
       console.log(JSON.stringify(require('./configdir/sample_deployments_response.js')));
       response.end(JSON.stringify(require('./configdir/sample_deployments_response.js')));
@@ -32,7 +32,7 @@ describe('library basic functions', function () {
   it('returns the fully unified correct configuration object, including system config', function (done) {
     var Apid = require('../lib/apid');
     var apidLib = new Apid();
-    apidLib.get({systemConfigPath: path.join(__dirname, 'configdir/systemConfig.yaml')}, (err, stitchedConfig) => {
+    apidLib.get({systemConfigPath: path.join(__dirname, 'configdir/systemConfig.yaml'), apidEndpoint: 'http://localhost:'+PORT}, (err, stitchedConfig) => {
       console.log('Unified config:\n', util.inspect(stitchedConfig, {depth: null }));
       //TODO do some checks here
       done();
