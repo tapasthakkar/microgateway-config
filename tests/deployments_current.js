840c7bfe9f1a55d8b2e8afdc7097e0774c0267dc
Matthew Dobson <mdobson4@gmail.com>
2017-02-23 10:42:40 -0500

    Updates to tests. Updating to piecemeal report only failed deployments compared to current state.

--- tests/deployments_current.js
+++ tests/deployments_current.js
@@ -7,37 +7,76 @@ var path = require('path')
 const util = require('util');
 const PORT=9090;
 
+var server;
+
+function createServer(handler, cb) {
+  var http = require('http');
+  
+  server = http.createServer(handler);
+  server.listen(PORT, function(){
+    console.log("Test apid server listening on: http://localhost:%s", PORT);
+    cb()
+  });
+}
+
 describe('library basic functions', function () {
-  before((done) => {
-    var http = require('http');
-    function handleRequest(request, response){
-      response.end(JSON.stringify(require('./configdir/sample_deployments_response.js')));
+  afterEach(() => {
+    if(server) {
+      server.close();
     }
-    var server = http.createServer(handleRequest);
-    server.listen(PORT, function(){
-      console.log("Test apid server listening on: http://localhost:%s", PORT);
-      done();
-    });
   })
 
   it('stitches together bundles correctly', function (done) {
-    var Apid = require('../lib/apid');
-    var apidLib = new Apid();
-    const stitchedConfig = apidLib.stitch(require('./configdir/sample_deployments_response.js'));
-    assert.equal(stitchedConfig, fs.readFileSync(path.join(__dirname, './output-expected')).toString());
-    done();
+
+    function handleRequest(request, response){
+      response.end(JSON.stringify(require('./configdir/sample_deployments_response.js')));
+    }
+
+    createServer(handleRequest, () => {
+      var Apid = require('../lib/apid');
+      var apidLib = new Apid();
+      const stitchedConfig = apidLib.stitch(require('./configdir/sample_deployments_response.js'));
+      assert.equal(stitchedConfig, fs.readFileSync(path.join(__dirname, './output-expected')).toString());
+      done();
+    })
   })
 
   it('returns the fully unified correct configuration object, including system config', function (done) {
-    var Apid = require('../lib/apid');
-    var apidLib = new Apid();
-    apidLib.get({systemConfigPath: path.join(__dirname, 'configdir/systemConfig.yaml'), apidEndpoint: 'http://localhost:'+PORT}, (err, stitchedConfig) => {
-      assert.equal(stitchedConfig.proxies.length, 4);
-      assert.equal(stitchedConfig.system.port, 8000);
-      assert.equal(stitchedConfig.system.vhosts.myvhost.vhost, 'www.myhost.com:9000');
-      assert.equal(stitchedConfig.system.vhosts.myvhost.cert, '/path/to/cert');
-      done();
-    });
+
+    
+    function handleRequest(request, response){
+      if(request.method == 'GET') {
+        response.end(JSON.stringify(require('./configdir/sample_deployments_response.js')));
+      } else {
+
+        var buf = [];
+
+        request.on('data', (d)=>{
+          buf += d;
+        });
+
+        request.on('end', () => {
+          var body = JSON.parse(buf.toString());
+          assert.equal(body.length, 4);
+          response.writeHead(200);
+          response.end();
+          done();
+        })
+        
+      }
+      
+    }
+
+    createServer(handleRequest, () => {
+      var Apid = require('../lib/apid');
+      var apidLib = new Apid();
+      apidLib.get({systemConfigPath: path.join(__dirname, 'configdir/systemConfig.yaml'), apidEndpoint: 'http://localhost:'+PORT}, (err, stitchedConfig) => {
+        assert.equal(stitchedConfig.proxies.length, 4);
+        assert.equal(stitchedConfig.system.port, 8000);
+        assert.equal(stitchedConfig.system.vhosts.myvhost.vhost, 'www.myhost.com:9000');
+        assert.equal(stitchedConfig.system.vhosts.myvhost.cert, '/path/to/cert');
+      });
+    })
   })
 
   it('calls back with an error if cannot connect to apid endpoint', function (done) {
@@ -48,4 +87,6 @@ describe('library basic functions', function () {
       done();
     });
   })
+
+
 });
