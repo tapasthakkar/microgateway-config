2184a8f2e94c400bc0051f6fe9a709974674898b
Matthew Dobson <mdobson4@gmail.com>
2017-03-10 13:25:09 -0500

    Updates to tests

---
+++ tests/configdir/sample_duplicate_config_response.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    "scopeId": "ABCDEF",
+    "id":"1234567890",
+    "displayName": "abc123",
+    "created":"1481917061",
+    "updated":"1481917061",
+    "createdBy":"mdobs",
+    "updatedBy":"mdobs",
+    "uri": "file://./tests/bundle1.zip",
+    "configuration": {
+      "PropBUNDLE_LEVEL": "scope1bundle2prop",
+      "PropSCOPE_LEVEL": "scope1prop",
+      "vhost": "myvhost"
+    }
+  },
+  {
+    "scopeId": "ABCDEF",
+    "id":"1234567891",
+    "displayName": "abc123_2",
+    "created":"1481917061",
+    "updated":"1481917061",
+    "createdBy":"mdobs",
+    "updatedBy":"mdobs",
+    "uri": "file://./tests/bundle1.zip",
+    "configuration": {
+      "PropBUNDLE_LEVEL": "scope1bundle1prop",
+      "PropSCOPE_LEVEL": "scope1prop",
+      "vhost": "myvhost"
+    }
+  }
+]
